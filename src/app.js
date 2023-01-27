84355dd44bb794e8a5df27fce0cdf10f6d1a669b
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-01-27 11:28:14 -0300

    Adicionada função para adicionar produtos de forma estática no banco

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import { openDbConnection } from "./config/database.js";
 import initializeExpressServer from "./config/express.js";
+import { registerSomeProducts } from "./services/RegisterProducts.js";
 
 openDbConnection(async (error) => {
     if (error) throw Error("Falha ao conectar ao banco de dados!");
-    else initializeExpressServer();
+    else {
+        initializeExpressServer();
+        await registerSomeProducts();
+    }
 });
