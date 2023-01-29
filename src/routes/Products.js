84c768313591e0d233eca2fd345e54301c3103a1
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-01-29 11:50:57 -0300

    Rota adicionada para obtenção de produto por ID

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { ObjectId } from "mongodb";
 
 import { getDbInstance } from "../config/database.js";
 
@@ -15,6 +16,26 @@ router.all("/api/get-products", async (req, res) => {
         roedores: products.filter(products => products.category === "roedores"),
         vale_presente: products.filter(products => products.category === "vale-presente"),
     });
+});
+
+router.all("/api/get-product/:id", async (req, res) => {
+    if (req.method !== "GET") return res.status(errors[405].code).send(errors[405]);
+    const { id } = req.params;
+    if (!id) {
+        errors[400].message = "missing product id in params";
+        return res.status(errors[400].code).send(errors[400]);
+    }
+    try {
+        const product = await getDbInstance().collection("products").findOne({_id: ObjectId(id)})
+        if (!product) {
+            errors[400].message = "product not found in server.";
+            return res.status(errors[400].code).send(errors[400]);
+        }
+        return res.status(200).send(product);
+    } catch (_) {
+        errors[400].message = "invalid id param.";
+        return res.status(errors[400].code).send(errors[400]);
+    }
 });
 
 export default router;
